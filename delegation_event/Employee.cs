4bae822dcab3ea9599cd2b618822fb0b654e8837
patrick <biggreyhairboy@gmail.com>
2022-09-11 12:58:34 +0800

    simple delegation

---
+++ delegation_event/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace delegation_event
+{
+    public class Employee
+    {
+        public Employee(string name, decimal salary)
+        {
+            this.Name = name;
+            this.Salary = salary;
+        }
+
+        public decimal Salary { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1:C}", Name, Salary);
+        }
+
+        public static bool CompareSalary(Employee e1, Employee e2)
+        {
+            return e1.Salary < e2.Salary;
+        }
+    }
+}
---
+++ delegation_event/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace delegation_event
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees =
+            {
+                new Employee("Bugs bunny", 20000),
+                new Employee("Elmer Fudd", 10000),
+                new Employee("Daffy Duck", 25000),
+                new Employee("Wile coyote", 1000000.38m),
+                new Employee("Foghorn Leghorn", 30000),
+                new Employee("RoadRunner", 50000)
+            };
+            BubbleSorter.Sort(employees, Employee.CompareSalary);
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+
+        }
+    }
+}
