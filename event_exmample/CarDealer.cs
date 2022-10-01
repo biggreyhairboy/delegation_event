8adba406e30d79a5732046f6b97cf3d99082b2b2
patrick <biggreyhairboy@gmail.com>
2022-10-01 15:24:05 +0800

    first commit and create

---
+++ event_exmample/CarDealer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace event_example
+{
+    public class CarInfoEventArgs : EventArgs
+    {
+        public CarInfoEventArgs(string car)
+        {
+            this.Car = car;
+        }
+
+        public string Car { get; set; }
+    }
+    
+    public class CarDealer
+    {
+        public delegate EventHandler<CarInfoEventArgs> newCarInfo();
+
+        public event EventHandler<CarInfoEventArgs> NewCarInfo;
+        // {
+        //     add
+        //     {
+        //         newCarInfo += value;
+        //     }
+        //     remove
+        //     {
+        //         newCarInfo -= value;
+        //     }
+        // }
+
+        public void NewCar(string car)
+        {
+            Console.WriteLine("CarDealer, new car{0}", car);
+            if (NewCarInfo != null)
+            {
+                NewCarInfo(this, new CarInfoEventArgs(car));
+            }
+        }
+    }
+}
